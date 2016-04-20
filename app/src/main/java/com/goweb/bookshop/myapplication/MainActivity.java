7bd0a2011e646387e9c14f2d7048701e80685be5
Batter2014 <ruslanbelinskyy@gmail.com>
2016-04-19 20:29:09 -0400

    initial project structure

---
+++ app/src/main/java/com/goweb/bookshop/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.goweb.bookshop.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                goToBooksActivity();
+            }
+        });
+    }
+
+    private void goToBooksActivity() {
+        Intent intent = new Intent(this, BooksActivity.class);
+        startActivity(intent);
+    }
+}
